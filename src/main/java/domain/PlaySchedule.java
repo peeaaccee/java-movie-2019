02f03e2c6eec28bdadb1b1a522768cc070a8af8a
JIHYEISWISDOM <wlgp8958@naver.com>
2022-12-05 16:33:06 +0900

    feat: Schedule 확인

--- src/main/java/domain/PlaySchedule.java
+++ src/main/java/domain/PlaySchedule.java
@@ -1,5 +1,7 @@
 package domain;
 
+import utils.DateTimeUtils;
+
 import java.time.LocalDateTime;
 
 import static utils.DateTimeUtils.format;
@@ -13,8 +15,33 @@ public class PlaySchedule {
         this.capacity = capacity;
     }
 
+    public boolean isValidCapacity(int capacity) {
+        if (this.capacity < capacity) {
+            throw new IllegalArgumentException("예약 인원 초과입니다.");
+        }
+        return true;
+    }
+
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public boolean isValidTime() {
+        // TODO
+        long time = System.currentTimeMillis();
+        return true;
+    }
+
+    public boolean isOneHourOverLap(PlaySchedule playSchedule) {
+        return DateTimeUtils.isOneHourWithinRange(this.startDateTime, playSchedule.startDateTime);
+    }
+
     @Override
     public String toString() {
         return "시작시간: " + format(startDateTime) + " 예약가능인원: " + capacity + "\n";
+    }
+
+    public String printSchedule() {
+        return "시작시간: " + format(startDateTime);
     }
 }
