ea929c00885e386be4da4969a75422eb6f1f5a71
JIHYEISWISDOM <wlgp8958@naver.com>
2022-12-05 18:49:46 +0900

    feat: MovieApplication

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,17 +1,105 @@
-import domain.Movie;
-import domain.MovieRepository;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
 import java.util.List;
 
 public class MovieApplication {
+
+    private static ReserveStatusList reserveStatusList = new ReserveStatusList();
+    private static TotalPrice totalPrice;
+    private static Point point;
+    private static PurchaseMean purchaseMean;
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
 
-        int movieId = InputView.inputMovieId();
+        selectMovie();
+        while (InputView.inputDoneOrNot() != 1) {
+            selectMovie();
+        }
 
+        OutputView.printResult(reserveStatusList);
+        point = getPoint();
+        totalPrice = new TotalPrice(calculateTotalPriceWithoutDiscount(reserveStatusList), point);
         // TODO 구현 진행
+        setTotalPrice();
+        OutputView.printTotalPrice(totalPrice);
     }
+
+    private static PurchaseMean setPurchaseMean() {
+        int purchaseMean = InputView.getPurchaseMean();
+
+        try {
+            return new PurchaseMean(purchaseMean);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return setPurchaseMean();
+        }
+    }
+
+    private static void setTotalPrice() {
+        totalPrice = new TotalPrice(calculateTotalPriceWithoutDiscount(reserveStatusList), point);
+    }
+
+    private static Point getPoint() {
+        int inputPoint = InputView.inputPoint();
+
+        try {
+            return new Point(inputPoint);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getPoint();
+        }
+    }
+
+    private static int calculateTotalPriceWithoutDiscount(ReserveStatusList reserveStatusList) {
+        return reserveStatusList.getTotalPrice();
+    }
+
+    private static void selectMovie() {
+        Movie movie = getMovie();
+        System.out.println(movie.toString());
+        PlaySchedule playSchedule = getSchedule(movie);
+        ReservePeople reservePeople = getPeople(playSchedule);
+
+        try {
+            reserveStatusList.putResult(new ReserveStatus(movie, playSchedule, reservePeople));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static Movie getMovie() {
+        try {
+            int id = InputView.inputMovieId();
+            return MovieRepository.getMovieWithId(id);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getMovie();
+        }
+    }
+
+    private static PlaySchedule getSchedule(Movie movie) {
+        int schedule = InputView.inputMovieSchedule();
+        try {
+            movie.checkValidSchedule(schedule);
+            return movie.getPlaySchedule(schedule);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getSchedule(movie);
+        }
+    }
+
+    private static ReservePeople getPeople(PlaySchedule playSchedule) {
+        int capacity = InputView.inputPeopleCapacity();
+        try {
+            playSchedule.isValidCapacity(capacity);
+            return new ReservePeople(capacity);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getPeople(playSchedule);
+        }
+    }
+
 }
